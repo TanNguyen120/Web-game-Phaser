dc4db4f64452ae79b2be1c4c2115de9ce69a4e4f
Taan <tannguyen1220@gmail.com>
2023-04-25 08:17:37 +0700

    play with animation

--- src/scenes/platformScene.ts
+++ src/scenes/platformScene.ts
@@ -6,7 +6,7 @@ import * as Phaser from 'phaser';
 export default class PlatForm extends Phaser.Scene {
   // Game object that we want in the game
   private megaman1: any;
-  private megaman2: any;
+  private hero: any;
   // you super to inherited all attribute and method of phaser scene (the parent class)
   constructor() {
     super('platform');
@@ -19,11 +19,16 @@ export default class PlatForm extends Phaser.Scene {
     //   'assets/sprite-sheet/megamanRun.png',
     //   'assets/sprite-sheet/megamanRun.json'
     // );
-    this.load.spritesheet('megamanRun', 'assets/sprite-sheet/megamanRun.png', {
-      frameWidth: 172,
-      frameHeight: 183,
-    });
-    this.load.image('megaman2', 'assets/megaman2.png');
+    this.load.atlas(
+      'megamanRun',
+      'assets/sprite-sheet/megamanRun.png',
+      'assets/sprite-sheet/megamanRun.json'
+    );
+    this.load.atlas(
+      'heroWalk',
+      'assets/sprite-sheet/heroWalk.png',
+      'assets/sprite-sheet/heroWalk.json'
+    );
     this.load.image('background', 'assets/menuBG.jpg');
   }
 
@@ -33,9 +38,46 @@ export default class PlatForm extends Phaser.Scene {
     //that mean  the first image will be back ground or low priority image that doesn`t want to be render all the time
     //and the last image should be the character or thing that need to render on top of other game object
     this.add.image(0, 0, 'background').setOrigin(0);
+
+    const frameNames = this.textures.get('megamanRun').getFrameNames();
+    console.log(frameNames);
+    //=================================================================================================
+    // also we have to create the animation for the sprite sheet
+    this.anims.create({
+      key: 'running',
+      frames: this.anims.generateFrameNames('megamanRun', {
+        start: 0,
+        end: 9,
+        zeroPad: 3,
+        prefix: 'tile',
+        suffix: '.png',
+      }),
+      frameRate: 9,
+      repeat: -1,
+    });
+
     this.megaman1 = this.add.sprite(400, 300, 'megamanRun');
-    this.megaman2 = this.add.image(100, 300, 'megaman2').setOrigin(0.5);
-    this.megaman2.setScale(0.3);
+
+    this.megaman1.play('running');
+    //=================================================================================================
+
+    //hero animate
+    this.anims.create({
+      key: 'walking',
+      frames: this.anims.generateFrameNames('heroWalk', {
+        start: 78,
+        end: 80,
+        zeroPad: 3,
+        prefix: 'tile',
+        suffix: '.png',
+      }),
+      frameRate: 14,
+      repeat: -1,
+    });
+
+    this.hero = this.add.sprite(400, 300, 'heroWalk');
+
+    this.hero.play('walking');
   }
 
   moveMegaMan(gameObject: any, speed: number): void {
@@ -51,6 +93,7 @@ export default class PlatForm extends Phaser.Scene {
 
   update() {
     // this.moveMegaMan(this.megaman1, 20);
-    this.moveMegaMan(this.megaman2, 3);
+    this.moveMegaMan(this.hero, 3);
+    this.moveMegaMan(this.megaman1, 9);
   }
 }
