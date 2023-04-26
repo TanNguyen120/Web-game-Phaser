2e54688309a8135ff3083e1912e60c0a890c51b4
Taan <tannguyen1220@gmail.com>
2023-04-26 13:02:29 +0700

    add physics to image object

--- src/scenes/platformScene.ts
+++ src/scenes/platformScene.ts
@@ -7,6 +7,7 @@ export default class PlatForm extends Phaser.Scene {
   // Game object that we want in the game
   private megaman1: any;
   private hero: any;
+  private sakuraEffect: any;
   // you super to inherited all attribute and method of phaser scene (the parent class)
   constructor() {
     super('platform');
@@ -19,17 +20,23 @@ export default class PlatForm extends Phaser.Scene {
     //   'assets/sprite-sheet/megamanRun.png',
     //   'assets/sprite-sheet/megamanRun.json'
     // );
+    //=================================================================================================
+
     this.load.atlas(
       'megamanRun',
       'assets/sprite-sheet/megamanRun.png',
       'assets/sprite-sheet/megamanRun.json'
     );
+    //=================================================================================================
+
     this.load.atlas(
       'heroWalk',
       'assets/sprite-sheet/heroWalk.png',
       'assets/sprite-sheet/heroWalk.json'
     );
     this.load.image('background', 'assets/menuBG.jpg');
+    //=================================================================================================
+    this.load.image('sakuraFlower', 'assets/effect/sakuraArt.png');
   }
 
   // THIS method use to add ALL GAME OBJECT THAT WILL BE DISPLAY ONCE THE SCENE IS CREATED
@@ -78,8 +85,21 @@ export default class PlatForm extends Phaser.Scene {
     this.hero = this.add.sprite(400, 300, 'heroWalk');
 
     this.hero.play('walking');
+    //=================================================================================================
+    //THE FALLING SAKURA EFFECT
+    // add physics group
+    this.sakuraEffect = this.physics.add.group();
+    // use the loop to assign object to the physics group
+    for (let index = 0; index < 5; index++) {
+      const element = this.physics.add.image(16, 0, 'sakuraFlower');
+      this.sakuraEffect.add(element);
+      element.setRandomPosition(0, 6);
+      element.setScale(0.3);
+      element.setAcceleration(100, 100);
+    }
   }
-
+  //=================================================================================================
+  // Function to make the game object move And re position when out of bound
   moveMegaMan(gameObject: any, speed: number): void {
     // random speed of the object
     gameObject.x += speed;
